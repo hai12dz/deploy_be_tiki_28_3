1c20102cfa1aee7567d5978b1399a8e9e21020b3
hai12dz <fifahayrataolahai1762004@gmail.com>
2025-04-11 17:21:16 +0700

    a

--- src/pages/client/home/category/category.list.tsx
+++ src/pages/client/home/category/category.list.tsx
@@ -56,24 +56,12 @@ const CategoryExplorer = () => {
                                     <img
                                         src="https://salt.tikicdn.com/cache/100x100/ts/ta/6c/37/a4/7ee5c72cc1c35b6b90b70b2ce3498215.png.webp"
                                         alt="Toggle"
-                                        style={{
-                                            width: "20px",
-                                            height: "20px",
-                                            transform: expandedCategories[item.label] ? "rotate(0deg)" : "rotate(180deg)",
-                                            transition: "transform 0.3s ease"
-                                        }}
+                                        className={expandedCategories[item.label] ? "arrow-expanded" : "arrow-collapsed"}
                                     />
                                 </div>
                             </div>
                             {expandedCategories[item.label] && nameCategory[item.label] && (
-                                <div
-                                    className="sc-36d678cb-5 cHTThk"
-                                    style={{
-                                        position: 'relative',
-                                        top: '-5px', // Move it up by 5px
-                                        marginBottom: '-5px' // Compensate for the upward movement
-                                    }}
-                                >
+                                <div className="sc-36d678cb-5 cHTThk dropdown-content">
                                     {nameCategory[item.label].map((subItem, subIndex) => (
                                         <div key={subIndex} className="sc-36d678cb-6 jZEauZ">
                                             <a href={`/subcategory/${subItem}`}>{subItem}</a>
